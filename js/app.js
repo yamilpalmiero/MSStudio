89a51ad6b7348b895739b134ba3a39a5030a5d9d
Yamil Palmiero <yamilpalmiero@gmail.com>
2024-01-06 18:33:58 +0100

    Pagina traducida al ingles

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-const hamburguesa = document.querySelector('.hamburguesa');
-const navegacion = document.querySelector('.navegacion');
-const enlaces = document.querySelectorAll('.navegacion a');
+const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
+const links = document.querySelectorAll('.navigation a');
 
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarMenu();
-    cerrarMenu();
+    showMenu();
+    closeMenu();
 });
 
 
@@ -13,18 +13,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //FUNCIONES
 /**
- * El elemento hamburguesa hace un evento de tipo click
+ * El elemento hamburger hace un evento de tipo click
  * Dicho elemento se va a disparar hasta que el usuario haga click
- * Esta funcion es para mostrar el menu de la hamburguesa u ocultarlo segun su situacion actual
+ * Esta funcion es para mostrar el menu de la hamburger o hide lo segun su situacion actual
  */
-function mostrarMenu() {
-    hamburguesa.addEventListener('click', () => { //El arrow-function es para que se ejecute hasta que el usuario interactue (haga click, en este caso)
-        //navegacion.classList.toggle('ocultar'); //Si navegacion contiene el elemento ocutar que lo saque (se muestran las opciones) y si no lo tiene lo agrega (las oculta)
-        if (navegacion.classList.contains('ocultar')) {
-            navegacion.classList.remove('ocultar');
+function showMenu() {
+    hamburger.addEventListener('click', () => { //El arrow-function es para que se ejecute hasta que el usuario interactue (haga click, en este caso)
+        //navigation.classList.toggle('hide'); //Si navigation contiene el elemento ocutar que lo saque (se muestran las opciones) y si no lo tiene lo agrega (las oculta)
+        if (navigation.classList.contains('hide')) {
+            navigation.classList.remove('hide');
         }
         else {
-            navegacion.classList.add('ocultar');
+            navigation.classList.add('hide')
         }
     });
 }
@@ -32,12 +32,12 @@ function mostrarMenu() {
 /**
  * Para cerrar el menu desplegado luego de que el usuario seleccione una de las opciones
  */
-function cerrarMenu() {
-    enlaces.forEach(enlace => {
-        enlace.addEventListener('click', (e) => {
+function closeMenu() {
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
             //console.log(e.target.tagName); //Para saber cual es el nombre de lo que se esta clickendo (la consola muestra 'A')
-            if (e.target.tagName === 'A') { //Si el usuario hizo click en un enlace, ocultar
-                navegacion.classList.add('ocultar');
+            if (e.target.tagName === 'A') { //Si el usuario hizo click en un enlace, lo oculto
+                navigation.classList.add('hide');
             }
         });
     });
